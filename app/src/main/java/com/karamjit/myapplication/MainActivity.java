78b4d66129d2c4bb748b0690ebf9f29a0287c6fc
karamjeet singh <karamjeet.zeroit@gmail.com>
2023-08-24 18:10:17 +0530

    2nd commit

--- app/src/main/java/com/karamjit/myapplication/MainActivity.java
+++ app/src/main/java/com/karamjit/myapplication/MainActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.karamjit.toasty.Toasty;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
